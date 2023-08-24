f1fd5ea55d468b5ca4567e9619c9e0baa3c69de6
N8Dawgg <natwheeler1337@gmail.com>
2023-08-24 12:56:34 -0400

    Created the 'bit grid' and implemented bit toggling.

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const SIZE_X = 16;
 const SIZE_Y = 16;
 
 const VBOX = document.querySelector(".vbox");
+let bits;
 
 function start() {
-    for (let y=0;y<SIZE_Y,y++){
+    for (let y=0;y<SIZE_Y;y++){
         let newHBox = document.createElement('div');
         newHBox.classList.add('hbox');
         VBOX.append(newHBox);
-        for (let x=0;x<SIZE_X,x++){
+        for (let x=0;x<SIZE_X;x++){
             let newBit = document.createElement('div');
             newBit.classList.add('bit');
             newHBox.append(newBit);
@@ -17,6 +18,12 @@ function start() {
 
     }
 
+    bits = document.querySelectorAll(".bit");
+    bits.forEach((bit) => bit.addEventListener('click', bitClicked));
+}
+
+function bitClicked(e){
+    e.target.classList.toggle("dark");
 }
 
 start()
